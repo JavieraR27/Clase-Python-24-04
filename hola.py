14107c77cd0d735055d4dcc47bcd6481255e4262
JavieraR27 <javieeeera.rojas@gmail.com>
2025-04-24 15:35:25 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,63 @@
+# print("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+
+# edad=int(input())
+# print("hola" , nombre , "su edad es " , edad)
+
+# if edad>=18:
+#     print("Usted es mayor de edad")
+# else:
+#     print("Usted no es mayor de edad")    
+
+# menor de 12 años es niño
+# entre 12 y 17 es adolescente
+# mayor de 18, es adulto
+# 65 y mas es adulto mayor
+
+# print("ingrese su edad")
+# edad=int(input())
+
+# if edad<12:
+#    print("usted es niño")
+# elif edad>=12 and edad<=17:
+#    print("Usted es adolecente")
+# elif edad>=18 and edad<=64:
+#    print("Usted es adulto")
+# else:
+#    print("Usted es adulto mayor")
+
+# clave=3344
+# pas=int(input("ingrese su clave:"))
+
+# if pas==clave:
+#    print("Su clave es correcta")
+# else:
+#    print("Su clave es incorrecta")
+
+
+# num=int(input("ingrese un numero"))
+
+# if num % 2==0:
+#     print("El numero es par")
+# else:
+#     print("El numero es impar")
+
+color=input("ingrese un color")
+
+# si es azul es de la u
+# si es blanco cc
+# si es rojo union
+# si es verde audax
+# si es otro no es color valido
+
+if color=="azul":
+    print("es de la u")
+elif color=="blanco":
+    print ("es del colo colo")
+elif color=="rojo":
+    print("es de union española")
+elif color=="verde":
+    print("es de audax")
+else:
+    print("no es un color valido")
---
+++ suma.py
@@ -0,0 +1,3 @@
+n1=int(input("Ingrese un numero"))
+n2=int(input("ingrese un numero"))
+print("El resultado es", n1+n2)
